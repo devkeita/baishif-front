959070a80b2206ca1a3161c400982c21aca1def9
devkeita <keita.mywork@gmail.com>
2020-11-23 14:47:51 +0900

    add api base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,7 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    baseURL: process.env.API_BASE_URL || 'http://127.0.0.1:8000/api',
   },
   auth: {
     redirect: {
